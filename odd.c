85e7bf6d2c2b761bc3992eb1f979f9043021aa1a
Ajithcholayil <30342463+Ajithcholayil@users.noreply.github.com>
2017-08-18 17:04:02 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+printf("Enter a integer value: ");
+scanf("%d",&value);
+if(number %2==0);
+printf("%d the number is even", number);
+else
+printf("%d" the number is odd", number);
+return 0;
+}
